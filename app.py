88fcaa5f91e8d2f22360e84ed0c109ad600cdd2d
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-11 21:12:37 +0500

    Работа декоратора

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask  import Flask
 
 app = Flask(__name__)
 
+@app.route("/index")
 @app.route("/")
 def index():
-    return "index"
+    return "<h1> ГЛАВНАЯ СТРАНИЦА</h1>"
+
+
+
+@app.route("/about")
+def about():
+    return "<h1> СТРАНИЦА О НАС</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
