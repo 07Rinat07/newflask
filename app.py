b58ea6287f64c460050d7a2f17b2082ff4f562d3
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-13 00:23:49 +0500

    Data Base----work with db and create to Python console command for create table and column db

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask  import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
+
+db = SQLAlchemy(app)
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String(300), nullable=False)
+    text= db.Column(db.Text, nullable=False)
 
 @app.route("/index")
 @app.route("/")
